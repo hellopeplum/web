5e590e938baae2d4833642e3d74c17db16c6f43a
Padam Sethia <padamsethia5@gmail.com>
2023-07-01 13:27:27 +0530

    added config

---
+++ pages/work/index.vue
@@ -0,0 +1,77 @@
+<template>
+    <div id="recent-works">
+      <section class=" py-6 my-6" :key="index" v-for="(row, index) in posts">
+        <div class="columns" :class="{ 'is-centered': row.shift == 'center' }">
+          <div class="column is-8" :class="{ 'is-offset-4-desktop': row.shift == 'right' }">
+            <figure class="image has-background-white-bis is-16by9">
+              <img class="" src="" alt="">
+            </figure>
+            <div class="columns is-centered mt-3">
+              <div class="column is-4-desktop is-12-touch">
+                <p class="title is-size-3-desktop is-size-4-touch">{{ row.title }}</p>
+                <p class="subtitle ">{{ row.time }}</p>
+              </div>
+              <div class="column is-6-desktop is-12-touch is-offset-2-desktop">
+                <p class="is-size-5-desktop is-size-6-touch">{{ row.description }}</p>
+                <br>
+                <nuxt-link v-if="row.link_preview" class="is-underlined is-size-5">See live →</nuxt-link>
+                <nuxt-link v-if="row.link_to_read" class="is-underlined is-size-5">Read more about project →</nuxt-link>
+              </div>
+            </div>
+          </div>
+        </div>
+        <br>
+        <br>
+        <br>
+      </section>
+      {{ posts }}
+      <br>
+      <br>
+    </div>
+  </template>
+  
+  <script setup>
+//   const data = [
+//     {
+//       'title': 'Atelier DS, handcrafted homewares',
+//       'description': "Through a sensitive mixed media approach, this musical number aims to help first\
+//                   responders by\
+//                   highlighting and visualizing the common feelings they have to deal with when doing their job. You won’t\
+//                   know whether to laugh or cry",
+//       'link_to_read': null,
+//       'link_preview': null,
+//       'year_of_work': '2016 - 2018',
+//       'shift': 'center', // left , right, center
+//     },
+//     {
+//       'title': 'Atelier DS, handcrafted homewares',
+//       'description': "Through a sensitive mixed media approach, this musical number aims to help first\
+//                   responders by\
+//                   highlighting and visualizing the common feelings they have to deal with when doing their job. You won’t\
+//                   know whether to laugh or cry",
+//       'link_to_read': null,
+//       'link_preview': null,
+//       'year_of_work': '2016 - 2018',
+//       'shift': 'left', // left , right, center
+//     },
+//     {
+//       'title': 'Atelier DS, handcrafted homewares',
+//       'description': "Through a sensitive mixed media approach, this musical number aims to help first\
+//                   responders by\
+//                   highlighting and visualizing the common feelings they have to deal with when doing their job. You won’t\
+//                   know whether to laugh or cry",
+//       'link_to_read': null,
+//       'link_preview': null,
+//       'year_of_work': '2016 - 2018',
+//       'shift': 'right', // left , right, center
+//     },
+//   ];
+  
+  const { data: posts } = await useAsyncData('work', () => queryContent('/work').only(['title','cover','description','time','shift']).find())
+  
+  // const { posts:blogposts } = await useAsyncData('work', () => {queryContent('work').findOne()})
+  
+  
+  </script>
+  
+  <style></style>
